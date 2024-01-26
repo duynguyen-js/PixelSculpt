556fd8aa3c81b747eb3514a75c5ec2c94457f13a
Duy-B-Nguyen <120677416+Duy-B-Nguyen@users.noreply.github.com>
2024-01-26 02:19:32 -0500

    add routing for home page and create post page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import {logo} from './assets/index'
+import Home from './components/home/Home'
+import CreatePost from './components/createPost/CreatePost'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link
+          to="/"
+          style={{ textDecoration: "none" }}
+          className="logo-container"
+        >
+          <img src={logo} alt="logo" className="logo" />
+          <h1>ImageX</h1>
+        </Link>
+
+        <Link to="/create-post">
+          <button className="create-button">Create</button>
+        </Link>
+      </header>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/create-post" element={<CreatePost />}></Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+}
+
+export default App
