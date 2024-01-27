3fe7c99aee239c793d56a6f6b9864bcece5d6426
Duy-B-Nguyen <duybnguyen04@gmail.com>
2024-01-27 11:54:50 -0500

    connected to openai

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-
+import dalleRoutes from './routes/dalleRoutes.js'
+import postRoutes from './routes/postRoutes.js'
 import cors from "cors";
 import connectDB from "./mongodb/connect.js";
 
@@ -12,13 +13,15 @@ const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use('/api/vi/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 // starts server
 const startServer = async () => {
 
   try {
     connectDB(process.env.MONGODB_URL)
-    app.listen(3000, () => {
+    app.listen(process.env.PORT, () => {
       console.log("Server is running on port", process.env.PORT);
     });
   } catch(error) {
